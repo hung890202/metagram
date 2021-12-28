0f6dd3b81e06767c978e6e95dc8b8554f6d02e6c
hung890202 <bir0413520@gmail.com>
2021-12-28 21:39:30 +0800

    add NFT page

---
+++ pages/mintedNFT/ANFTStyles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const StyledANFT = styled.div`
+    display: grid;
+  color: #ffffff;
+  background-color: #dddddd46;
+  height: 33.5rem;
+  padding: 0.625rem;
+  box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 0.3);
+  border-radius: 0.5rem;
+  align-content: center;
+`;
+export const ANFTText =styled.div`  
+    text-algin:left;
+    padding:0rem 1rem;
+    margin:0rem 1rem;
+    box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 0.3);
+    border-radius: 0.5rem;
+`
+export const ANFTImage = styled.image`
+    height: 23.75rem;
+    width: 23.75rem;
+    margin:1rem;
+    object-fit: cover;
+    border-radius: 0.625rem;
+    align-content: center;
+    box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 0.3);
+`
+export const ANFTAddress =styled.div`
+    text-algin:left;
+    height:2rem;
+    padding:0rem 1rem;
+    margin:0rem 1rem;
+    box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 0.3);
+    border-radius: 0.5rem;
+   
+`
